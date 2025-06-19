d416b7a23b2ad8c9fd368160adcbb22e6ef76539
Neil <neil@ingenuity.ph>
2025-06-19 09:44:54 +0800

    chore: setup storybook

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { sheriff, tseslint } from "eslint-config-sheriff";
 import pluginQuery from "@tanstack/eslint-plugin-query";
 import pluginRouter from "@tanstack/eslint-plugin-router";
