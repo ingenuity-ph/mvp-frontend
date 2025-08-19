a4a6604fe392625537b656d67d25c0b95230605e
Neil <neil@ingenuity.ph>
2025-08-19 14:16:34 +0800

    chore: updated test setup

--- setupTests.ts
+++ setupTests.ts
@@ -1,11 +1,5 @@
 import * as matchers from "@testing-library/jest-dom/matchers";
-import { cleanup } from "@testing-library/react";
-import { afterEach, expect } from "vitest";
+import { expect } from "vitest";
 
 // Extend expect with jest-dom matchers
 expect.extend(matchers);
-
-// Cleanup after each test
-afterEach(() => {
-  cleanup();
-});
